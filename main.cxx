76495ace3aaa52652e0e4bc74a56a02215e2c888
Filippo Valle <filippo.valle@edu.unito.it>
2018-12-10 15:22:22 +0100

    main

--- main.cxx
+++ main.cxx
@@ -14,18 +14,18 @@ int main(){
     std::vector<ConfigSimulation> configs;
     //3.1
     configs.emplace_back(ConfigSimulation(false, 100, 2000, false, 0., nullptr));
-    configs.emplace_back(ConfigSimulation(false, 100, 10000, false, 0.3, [&](){return gRandom->Gaus(0, 0.3);}));
-    configs.emplace_back(ConfigSimulation(false, 100, 10000, false, 0.4, [&](){return gRandom->Gaus(0, 0.4);}));
-    configs.emplace_back(ConfigSimulation(false, 100, 10000, false, 0.8, [&](){return gRandom->Gaus(0, 0.8);}));
-    configs.emplace_back(ConfigSimulation(false, 100, 10000, false, 1, [&](){return gRandom->Gaus(0, 1);}));
+    for(double sigma = 0; sigma < 2.5; sigma+=0.1) configs.emplace_back(ConfigSimulation(false, 100, 10000, false, sigma, [&](){return gRandom->Gaus(0, sigma);}));
 
     //3.2
+    configs.emplace_back(ConfigSimulation(true, 100, 10000, false, 0, nullptr));
+    for(double sigma = 0; sigma < 2.5; sigma+=0.1) configs.emplace_back(ConfigSimulation(true, 100, 10000, false, sigma, [&](){return gRandom->Gaus(0, sigma);}));
+
+    configs.emplace_back(ConfigSimulation(false, 100, 2000, true, 0., nullptr));
+    for(double sigma = 0; sigma < 2.5; sigma+=0.1) configs.emplace_back(ConfigSimulation(false, 100, 10000, true, sigma, [&](){return gRandom->Gaus(0, sigma);}));
+
     configs.emplace_back(ConfigSimulation(true, 100, 10000, true, 0, nullptr));
-    configs.emplace_back(ConfigSimulation(true, 100, 10000, true, 0.3, [&](){return gRandom->Gaus(0, 0.3);}));
-    configs.emplace_back(ConfigSimulation(true, 100, 10000, true, 0.4, [&](){return gRandom->Gaus(0, 0.4);}));
-    configs.emplace_back(ConfigSimulation(true, 100, 10000, true, 0.8, [&](){return gRandom->Gaus(0, 0.8);}));
-    configs.emplace_back(ConfigSimulation(true, 100, 10000, true, 1, [&](){return gRandom->Gaus(0, 1);}));
-    configs.emplace_back(ConfigSimulation(true, 100, 10000, true, 0.4, [&](){return gRandom->Rndm()*0.8-0.4;}));
+    for(double sigma = 0; sigma < 2.5; sigma+=0.1) configs.emplace_back(ConfigSimulation(true, 100, 10000, true, sigma, [&](){return gRandom->Gaus(0, sigma);}));
+
 
     auto MyAnalizer = Analyzer::Instance(configs);
     MyAnalizer->RunAnalyzer();
